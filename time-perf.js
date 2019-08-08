946e0b2b085e082073666c2cd383c4baa34e9e58
Rémy <remy.drouet@sogelink.fr>
2019-08-08 10:30:34 +0200

    2.0.0
    Convert to class with node 12 features

--- time-perf.js
+++ time-perf.js
@@ -1,17 +1,30 @@
+"use strict"
+
+/**
+ * @namespace TimePerf
+ *
+ * @typedef {object} Step
+ * @property {number} time
+ * @property {string} name
+ * @property {TimePerf} [child]
+ */
+
 /**
  * TimePerf is a simple node module to evaluate the performance of a function|application|algorithm
  * @constructor
  * @property {Step[]} steps - Each recorded step
+ * @property {object<*, TimePerf>} instances
+ *
  * @example
- * var timePerf = require('time-perf');
+ * import timePerf from "time-perf"
  *
- * timePerf.start();
+ * timePerf.start()
  * // ..execute algorithm to evaluate
- * timePerf.step('Algorithm');
+ * timePerf.step("Algorithm")
  * // ..execute a function to evaluate
- * timePerf.stop('Function');
+ * timePerf.stop("Function")
  *
- * timePerf.print();
+ * timePerf.print()
  * // Will display :
  * --------------------------------
  * -          TimePerf result         -
@@ -22,33 +35,28 @@
  *  > 1. Algorithm   : 15.23 %
  *  > 2. Function    : 84.77 %
  */
-function TimePerf(depth){
-	/**
-	 * @typedef {object} Step
-	 * @property {number} time
-	 * @property {string} name
-	 * @property {TimePerf} [child]
-	 */
-	this.steps = [];
-	this.pauseStep = 0;
-	this.pauseTime = 0;
-	this.result = 0;
-	this.strResult = "TimePerf created";
-	this.depth = depth?depth:0;
-	this.name = 'TimePerf session '+this.depth;
-	this.children = [];
-	this.tag = "[TimePerf] ";
-	this.instances = {};
-	return this;
-}
+class TimePerf {
+	steps = []
+	pauseStep = 0
+	pauseTime = 0
+	result = 0
+	strResult = "TimePerf created"
+	children = []
+	tag = "[TimePerf] "
+	instances = {}
 
-TimePerf.prototype = {
+	constructor(depth) {
+		this.depth = depth ? depth : 0
+		this.name = `TimePerf session ${ this.depth }`
+		return this
+	}
+
 	/**
 	 * Remove all created instances
 	 */
-	cleanInstances: function (){
-	    this.instances = {};
-	},
+	cleanInstances() {
+		this.instances = {}
+	}
 
 	/**
 	 * Create a new TimePerf instance and store it in the instance map
@@ -57,7 +65,7 @@ TimePerf.prototype = {
 	 * @example <caption>Create a new TimePerf instance</caption>
 	 * // Better use startInstance and stopInstance for simple usages
 	 * // alias getInstance both will return the instance if it exists or create it if not
-	 * const instance = timePerf.newInstance("instance1");
+	 * const instance = timePerf.newInstance("instance1")
 	 * if (instance.pauseStep)
 	 *  instance.unpause()
 	 * else
@@ -67,16 +75,16 @@ TimePerf.prototype = {
 	 * // end
 	 * instance.stop().print()
 	 */
-	newInstance: function (name){
-	    if (!this.instances[name]) {
-		    this.instances[name] = new TimePerf();
-	    }
-        return this.instances[name];
-	},
+	newInstance(name) {
+		if (!this.instances[name]) {
+			this.instances[name] = new TimePerf()
+		}
+		return this.instances[name]
+	}
 
-	getInstance: function () {
-		return this.newInstance.apply(this,arguments)
-	},
+	getInstance() {
+		return this.newInstance.apply(this, arguments)
+	}
 
 	/**
 	 * Create a new instance or get it if it exists and start/unpause it.
@@ -85,55 +93,56 @@ TimePerf.prototype = {
 	 * @returns {TimePerf}
 	 * @example <caption>Instance simple usage</caption>
 	 * // At the begining of the function to measure
-	 * timePerf.startInstance("Function duration");
+	 * timePerf.startInstance("Function duration")
 	 * // At the end of it
-	 * const endCondition = index === lastIndex;
-	 * timePerf.stopInstance("Function duration", endCondition);
+	 * const endCondition = index === lastIndex
+	 * timePerf.stopInstance("Function duration", endCondition)
 	 */
-	startInstance: function (name){
-		const tpInst = this.getInstance(name);
+	startInstance(name) {
+		const tpInst = this.getInstance(name)
 
 		if (tpInst.pauseStep) {
-			tpInst.unpause();
+			tpInst.unpause()
 		} else {
-			tpInst.start(name);
+			tpInst.start(name)
 		}
 
-		return tpInst;
-	},
+		return tpInst
+	}
 
 	/**
 	 * Switch to pause the instance or stop, print and clean it when the endCondition is true.
 	 * It is recommended to use stopInstance with startInstance only
 	 * @param {string}  name         - The id of the instance
-	 * @param {boolean} endCondition - When the instance is stopped
+	 * @param {boolean} [endCondition=true] - When the instance is stopped
 	 * @returns {TimePerf}
 	 */
-	stopInstance: function (name, endCondition){
-		const tpInst = this.getInstance(name);
+	stopInstance(name, endCondition = true) {
+		const tpInst = this.getInstance(name)
 		if (endCondition) {
-			tpInst.stop().print();
-			delete this.instances[name];
+			tpInst.stop().print()
+			delete this.instances[name]
 		} else {
-			tpInst.pause();
+			tpInst.pause()
 		}
-		return tpInst;
-	},
+		return tpInst
+	}
 
 	/**
 	 * Reset TimePerf & mark a step in TimePerf measurements associated with a given name
 	 * @param {String} name - Name of the new TimePerf session
 	 * @returns {TimePerf} timePerf - The TimePerf Object
 	 */
-	start: function (name) {
-		if(name){
-			this.name = name;
+	start(name) {
+		if (name) {
+			this.name = name
 		}
-		this.reset().step();
-		this.result = 0;
-		this.strResult = this.tag+this.name+" started";
-		return this;
-	},
+		this.reset().step()
+		this.result = 0
+		this.strResult = this.tag + this.name + " started"
+		return this
+	}
+
 	/**
 	 * Mark a step in TimePerf measurements associated with a given name and switch TimePerf to pause
 	 * You can use {@link resume} just after to show a resume
@@ -141,33 +150,34 @@ TimePerf.prototype = {
 	 * @param [name] - Name associated to the step
 	 * @returns {TimePerf} timePerf - The TimePerf Object
 	 */
-	stop: function (name) {
-		this.step(name?name:this.name).pause();
-		this.result = 0;
-		this.strResult = this.tag+this.name+" stopped";
-		return this;
-	},
+	stop(name) {
+		this.step(name ? name : this.name).pause()
+		this.result = 0
+		this.strResult = this.tag + this.name + " stopped"
+		return this
+	}
+
 	/**
 	 * Mark a step in TimePerf measurements associated with a given name
 	 * You can also use {@link resume} and {@link pause} to after
 	 * @param {string} [name] - Name associated to the step
 	 * @returns {TimePerf} timePerf - The TimePerf Object
 	 */
-	step: function (name) {
-		if(this.pauseStep){
-			console.log("/!\\ "+this.tag+this.name+" can't mark a step during a pause or when it's stopped ("+name+" step has been ignored)");
-			this.result = 0;
-			return this;
+	step(name) {
+		if (this.pauseStep) {
+			console.error("/!\\ " + this.tag + this.name + " can't mark a step during a pause or when it's stopped (" + name + " step has been ignored)")
+			this.result = 0
+			return this
 		}
-		if(!name){
-			name = "Step "+this.steps.length+1;
+		if (!name) {
+			name = "Step " + this.steps.length + 1
 		}
-		this.steps.push({time:Date.now()-this.pauseTime,name: name, children:this.children});
-		this.children = [];
-		this.result = this.steps.length-1;
-		this.strResult = "[TimePerf] > "+this.result+". "+name;
-		return this;
-	},
+		this.steps.push({ time: Date.now() - this.pauseTime, name: name, children: this.children })
+		this.children = []
+		this.result = this.steps.length - 1
+		this.strResult = "[TimePerf] > " + this.result + ". " + name
+		return this
+	}
 
 	/**
 	 * Get the result of the measurements (alias resume)
@@ -188,192 +198,195 @@ TimePerf.prototype = {
 	 * @example <caption>Resume one step silently</caption>
 	 * // returns the second step percentage without console messages
 	 * timePerf.print(2,true);
-	 * @returns {TimePerf} timePerf - The TimePerf Object plus a percentage or an array of percentage (timePerf.result == 0 for error)
+	 * @returns {TimePerf} timePerf - The TimePerf Object plus a percentage or an array of percentage (timePerf.result
+	 *     == 0 for error)
 	 */
-	print: function (index,silent) {
-		if(this.steps.length<2){
-			this.strResult = indent+"\n/!\\ TimePerf did not find enough steps too display a result";
-			if(this.steps.length==1)
-				this.strResult+= indent+"\n\t> TimePerf is started but never stepped/stopped\n";
+	print(index, silent) {
+		if (this.steps.length < 2) {
+			this.strResult = `${indent}\n/!\\ TimePerf did not find enough steps too display a result`
+			if (this.steps.length == 1)
+				this.strResult += `${indent}\n\t> TimePerf is started but never stepped/stopped\n`
 			else
-				this.strResult+= indent+"\n\t> TimePerf has not been started\n";
-			this.result = [];
+				this.strResult += `${indent}\n\t> TimePerf has not been started\n`
+			this.result = []
 		} else {
-			var indent = '';
+			var indent = ""
 			for (var i = 0; i < this.depth; i++) {
-				indent += '\t';
+				indent += "\t"
 			}
 			if (arguments.length == 1 && arguments[0] === true) {
-				silent = true;
-				index = null;
+				silent = true
+				index = null
 			}
 			if (!index) {
-				if( typeof this.result === 'number' && this.result>0)
-					index =this.result;
-				else if(this.depth >0)
-					index=this.steps.length-1;
+				if (typeof this.result === "number" && this.result > 0)
+					index = this.result
+				else if (this.depth > 0)
+					index = this.steps.length - 1
 			}
 			if (index && index > 0 && index < 2) {
-				this.result = this.steps[index].time - this.steps[index - 1].time;
-				this.strResult = indent + "> " + this.steps[index].name + "\t: " + this.result + " ms\n";
+				this.result = this.steps[index].time - this.steps[index - 1].time
+				this.strResult = indent + "> " + this.steps[index].name + "\t: " + this.result + " ms\n"
 			} else {
-				var nbSteps = this.steps.length - 1;
-				var testDuration = this.steps[nbSteps].time - this.steps[0].time;
-				var strResult = '';
-				if (this.depth == 0) {
-					strResult += "--------------------------------\n";
-					strResult += "-         TimePerf result      -\n";
-					strResult += "--------------------------------\n";
+				const nbSteps = this.steps.length - 1
+				const testDuration = this.steps[nbSteps].time - this.steps[0].time
+				let strResult = ""
+				if (this.depth === 0) {
+					strResult += "┌─────────────────────────────┐\n"
+					strResult += "│       TimePerf result       │\n"
+					strResult += "├─────────────────────────────┘\n"
 				}
-				strResult += indent + '## '+this.name + "\n";
+				strResult += `│${indent} « ${this.name} »\n`
 				//strResult    += indent+"+ Total duration  : "+(this.pauseTime+testDuration)+" ms\n";
-				strResult += indent + "+ TimePerf duration : " + testDuration + " ms\n";
-				strResult += indent + "+ Pause duration    : " + this.pauseTime + " ms\n";
-				var percentage, percentages = [];
-				for (var i = 1; i < this.steps.length; i++) {
-					var step = this.steps[i];
-					if(i>0){
-						var iDuration = step.time - this.steps[i - 1].time;
-						percentage = 100 * iDuration / testDuration;
-						percentages.push(percentage);
-						strResult += indent + "> " + i + ". " + step.name + "\t: " + percentage.toFixed(2) + " %\t(" + iDuration + " ms)\n";
-					}
-					for (var j = 0; j < step.children.length; j++) {
-						strResult += step.children[j].print(true).strResult;
+				strResult += `│${indent}▐ TimePerf duration : ${testDuration} ms\n`
+				strResult += `│${indent}▐ Pause duration    : ${this.pauseTime} ms\n`
+				let percentage
+				const percentages = []
+				for (let i = 1; i < this.steps.length; i++) {
+					const step = this.steps[i]
+
+					const iDuration = step.time - this.steps[i - 1].time
+					percentage = 100 * iDuration / testDuration
+					percentages.push(percentage)
+					strResult += `│${indent} ■ ${i}. ${step.name}\t: ${percentage.toFixed(2)} %\t (${iDuration} ms)\n`
+
+					for (let j = 0; j < step.children.length; j++) {
+						strResult += step.children[j].print(true).strResult
 					}
 				}
-				this.result = percentages;
-				this.strResult = strResult;
+				this.result = percentages
+				this.strResult = strResult
 			}
 		}
-		return silent?this:this.log();
-	},
+		return silent ? this : this.log()
+	}
 
-	resume: function () {
-		this.print.apply(this,arguments)
-	},
+	resume() {
+		this.print.apply(this, arguments)
+	}
 
 	/**
 	 * Resets the TimePerf tool (Removes all steps)
 	 * @returns {TimePerf} timePerf - The TimePerf Object
 	 */
-	reset: function () {
-		this.steps = [];
-		this.pauseStep = 0;
-		this.pauseTime = 0;
-		this.result = 0;
-		this.strResult = "TimePerf reset";
-		return this;
-	},
+	reset() {
+		this.steps = []
+		this.pauseStep = 0
+		this.pauseTime = 0
+		this.result = 0
+		this.strResult = "TimePerf reset"
+		return this
+	}
 
 	/**
 	 * Switch TimePerf to pause. You can't mark a step during a pause.
 	 * pause doesn't modify the previous action result and strResult
 	 * @example
-	 * timePerf.step("Step name").pause().print();
-	 * //Will display
+	 * timePerf.step("Step name").pause().print()
+	 * // Will display
 	 * "[TimePerf] > 1. Step name : 127 ms"
 	 * @returns {TimePerf} timePerf - The TimePerf Object
 	 */
-	pause: function () {
-		this.pauseStep = Date.now();
-		return this;
-	},
+	pause() {
+		this.pauseStep = Date.now()
+		return this
+	}
 
 	/**
 	 * Unpauses TimePerf and returns the pause duration
 	 * @example <caption>chain log with unpause</caption>
-	 * timePerf.unpause().log();
+	 * timePerf.unpause().log()
 	 * // will display
 	 * "TimePerf has marked a 20 ms pause"
 	 * @returns {TimePerf} scope - The TimePerf object plus the last pause duration
 	 */
-	unpause: function () {
-		var res = 0;
-		if(this.pauseStep){
-			res = Date.now() - this.pauseStep;
-			this.pauseTime+= res;
-			this.strResult = this.tag+this.name+" has marked a "+res+" ms pause";
+	unpause() {
+		let res = 0
+		if (this.pauseStep) {
+			res = Date.now() - this.pauseStep
+			this.pauseTime += res
+			this.strResult = `${this.tag}${this.name} has marked a ${res} ms pause`
 		} else {
-			this.strResult = "/!\\ "+this.tag+this.name+" has not been paused before to unpause.";
+			this.strResult = `/!\\ ${this.tag}${this.name} has not been paused before to unpause.`
 		}
-		this.pauseStep = 0;
-		this.result = res;
-		return this;
-	},
+		this.pauseStep = 0
+		this.result = res
+		return this
+	}
 
 	/**
 	 * Display the last result as a message in the console
 	 * @returns {TimePerf}
 	 */
-	log: function () {
-		if(this.strResult)
-			console.log(this.strResult);
-		return this;
-	},
+	log() {
+		if (this.strResult)
+			console.log(this.strResult)
+		return this
+	}
 
 	/**
 	 * Create a new TimePerf child. This child will be bind to the next step
 	 * @returns {TimePerf} child - The created child
 	 */
-	child: function () {
-		if(this.steps.length>0){
-			var newChild = new TimePerf(this.depth+1);
-			this.children.push(newChild);
-			return newChild;
+	child() {
+		if (this.steps.length > 0) {
+			const newChild = new TimePerf(this.depth + 1)
+			this.children.push(newChild)
+			return newChild
 		} else {
-			console.log("TimePerf hasn't been started. Child can't be created without available parent");
+			console.log("TimePerf hasn't been started. Child can't be created without available parent")
 		}
-	},
+	}
 
 	/**
 	 * Create and start a new TimePerf child. This child will be bind to the next
 	 * @param {string} name - Child's name
 	 * @returns {TimePerf} child - The created child
 	 */
-	childStart: function (name) {
-		return this.child().start(name);
-	},
+	childStart(name) {
+		return this.child().start(name)
+	}
 
 	/**
 	 * Stop the last TimePerf child
 	 * @param {string} [name] - Name of the last child step (otherwise will be the child name)
 	 * @returns {TimePerf} parent - Return the current TimePerf (not the child)
 	 */
-	childStop: function(name){
-		if(this.children.length > 0) {
-			var child = this.children[this.children.length-1];
-			child.stop(name);
+	childStop(name) {
+		if (this.children.length > 0) {
+			const child = this.children[this.children.length - 1]
+			child.stop(name)
+		} else {
+			console.log(`${this.tag}No child has been started yet`)
 		}
-		else
-			console.log(this.tag+"No child has been started yet");
-		return this;
-	},
+		return this
+	}
 
 	/**
 	 * Return the last created child for the current step, if none returns the current TimePerf.
 	 * @returns {TimePerf} child
 	 */
-	lastChild: function () {
-		if(this.children.length > 0) {
-			return this.children[this.children.length-1];
+	lastChild() {
+		if (this.children.length > 0) {
+			return this.children[this.children.length - 1]
 		}
-		console.log(this.tag+"No child has been started yet");
-		return this;
-	},
+		console.log(`${this.tag}No child has been started yet`)
+		return this
+	}
 
 	/**
 	 * Return the duration (in ms) of a specific step
 	 * @param {number} index - Step's index
 	 * @returns {number}
 	 */
-	getTime: function (index) {
-		if(index>0 && index<this.steps.length){
-			return this.steps[index].time-this.steps[index-1].time;
+	getTime(index) {
+		if (index > 0 && index < this.steps.length) {
+			return this.steps[index].time - this.steps[index - 1].time
 		} else {
-			console.log(index + "index does not exist");
+			console.log(`${index} index does not exist`)
 		}
 	}
-};
+}
 
-module.exports = new TimePerf();
+module.exports.TimePerf = TimePerf
+module.exports = new TimePerf()
